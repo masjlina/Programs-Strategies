73d35a59c0e21a7ae2a71f04b00686303865c1ec
MasJlina <anton.omelch@gmail.com>
2026-05-27 18:46:46 +0200

    apply migration

--- server/WebAPI/Program.cs
+++ server/WebAPI/Program.cs
@@ -85,4 +85,10 @@ app.UseCors("ProgramsStrategies");
 app.UseHttpsRedirection();
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
